1d61fb65bb263eab4e9ccbbc0ed37030c455fca1
Joel Quimper <joel_quimper@hotmail.com>
2025-12-10 17:42:22 -0500

    ajout de la configuration de lancement et des tâches pour l'API Agent et la console Expert

--- Start-Devtunnel.ps1
+++ Start-Devtunnel.ps1
@@ -1,3 +1,8 @@
 $tunnelName = "tiguidou-jq"
+
+#only first time when creating the tunnel
+#devtunnel create $tunnelName --allow-anonymous
+#devtunnel port create -p 7130 --protocol https
+
 devtunnel update $tunnelName --expiration 30d
 devtunnel host $tunnelName
